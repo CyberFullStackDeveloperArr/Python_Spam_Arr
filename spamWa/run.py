deb30fe0e5fd35e4d84c030d291fcc895aed542b
Ghost_Code_Night <127316255+CyberFullStackDeveloperArr@users.noreply.github.com>
2024-01-27 11:39:10 +0700

    run.py

--- spamWa/run.py
+++ spamWa/run.py
@@ -9,9 +9,7 @@ for a in range(5000):
     time.sleep(0.1)
     pyautogui.typewrite(["enter"])
 
-#python run.py enter pada terminal tapi crusor pada pesan wasap
-#jadi ketip" krusor tetap pada terminal tetapi krusor nya pada cht wasap yang di tuju
-#https://youtu.be/Xyl7uRdPoq0?si=vhnK1-JxMC8XbOp9 
+
     
 #pip install pyautogui
 #python run.py
